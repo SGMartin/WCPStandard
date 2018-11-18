5ac19c23982aa059f2fbfaa8f8ae9d79a3ec5be7
Darkr4ptor <wrpro2@gmail.com>
2018-11-18 02:25:39 +0100

    More typos with project name, now it compiles

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Authentication
 {
@@ -6,7 +7,15 @@ namespace Authentication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+         Console.Title = "「Starting」Authentication server";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(@" _______        _______ _______  ______ _______ _______ _     _");
+            Console.WriteLine(@" |_____| |         |    |______ |_____/ |______ |  |  | |     |");
+            Console.WriteLine(@" |     | |_____    |    |______ |    \_ |______ |  |  | |_____|");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(new string('_', Console.WindowWidth));
+            Console.WriteLine();
+
         }
     }
 }

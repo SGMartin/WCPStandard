fd96b047d75ac9f64e6b17163585d9b38d788a2b
Santiago <wrpro2@gmail.com>
2018-11-22 13:07:08 +0100

    Basic CMD parser. CFG file path can be changed using console arguments.

---
+++ Authentication/Options.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *                                                            Option.cs manages command line arguments
+ * 
+ *                                                      The command line is parsed using Eric Newton´s CLR parser
+ *                                                        https://github.com/commandlineparser/commandline                                                                                  
+ * 
+ * 
+ * 
+*/
+using System.Collections.Generic;
+using CommandLine;
+
+namespace Authentication
+{
+    class Options
+    {
+        [Option('c', "config", Required = false, HelpText = "Specify custom config file.")]
+        public string InputCFG { get; set; }
+
+    }
+}
